66f91b9674fe5f6249e600ae4a46e2f4df3c5754
Giovanni Ribeiro <giovanni_ge@hotmail.com>
2026-09-15 16:23:52 -0300

    Update 11-exercicio-placar-futebol.py

--- 01-Definicao-Literal/11-exercicio-placar-futebol.py
+++ 01-Definicao-Literal/11-exercicio-placar-futebol.py
@@ -4,16 +4,16 @@
 # Descrição:
 # Crie um programa que represente o resultado de uma partida de futebol.
 #  O programa deve armazenar o nome de dois times 
-#  e a quantidade de golos marcados por cada equipa.
+#  e a quantidade de gols marcados por cada equipa.
 
-# Depois, apresente o placar e calcule o total de golos da partida.
+# Depois, apresente o placar e calcule o total de gols da partida.
 
 # Resultado no terminal
 # ================================
 #         RESULTADO DO JOGO
 # ================================
 # França 3 x 2 Espanha
-# Total de golos: 5
+# Total de gols: 5
 # ================================
 
 time_casa = "França"
@@ -30,6 +30,6 @@ print("================================")
 
 print(f"{time_casa} {gols_casa} x {gols_visitante} {time_visitante}")
 
-print(f"Total de golos: {total_gols}")
+print(f"Total de gols: {total_gols}")
 
 print("================================")
