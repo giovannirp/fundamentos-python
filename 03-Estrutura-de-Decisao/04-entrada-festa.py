abceb70cc34b3538882d7d8da9ef011d164cae6d
Giovanni <giovanni_ge@hotmail.com>
2026-09-16 16:44:58 -0300

    feat: adicionando aplicação nota

--- 03-Estrutura-de-Decisao/04-entrada-festa.py
+++ 03-Estrutura-de-Decisao/04-entrada-festa.py
@@ -1,10 +1,15 @@
+# 18 anos ou mais: Pode entrar na festa.
+# 16 ou 17 anos: Pode entrar com responsável.
+# Menos de 16 anos: Não pode entrar na festa.
+
 # Entrada de dados do usuário
 idade = int(input("Digite sua idade: "))
 
-# Verificação da idade
 if idade >= 18:
     print("Pode entrar na festa!")
+
 elif idade >= 16:
     print("Pode entrar com responsável.")
+
 else:
     print("Não pode entrar na festa.")
---
+++ 03-Estrutura-de-Decisao/05-aplicacao-nota.py
@@ -0,0 +1,16 @@
+# Crie um programa que peça a nota do aluno e informe se ele foi aprovado ou reprovado.
+
+# Se a nota for maior ou igual a 7, o aluno está aprovado.
+
+# Caso contrário, o aluno está reprovado.
+
+# Utilize if e else.
+
+# Entrada de dados do usuário
+nota = float(input("Digite sua nota: "))
+
+# Verificação da nota
+if nota >= 7:
+    print("Aluno aprovado!")
+else:
+    print("Aluno reprovado!")
